f6f72b4204c30b360981874b6c775e1adc5b9153
AnetByju456 <anetchirayath333@gmail.com>
2026-08-05 19:48:56 +0530

    11.12-Restore health check

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  //second time
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw ('error...')
   res.send('ok')
 })
 
